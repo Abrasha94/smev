df3e8c6da3877664e7c77a8f0dce7c8f5e1d10c6
Abrasha94 <topdisplay4ik@gmail.com>
2023-01-02 13:20:13 +0300

    init commit

---
+++ smev/src/main/java/ru/intervale/smev/SmevApplication.java
@@ -0,0 +1,13 @@
+package ru.intervale.smev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmevApplication.class, args);
+    }
+
+}
